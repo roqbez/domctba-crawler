e9fbbdab5ee789fd99bd0ce69c1e58f383f6bae4
Roque Bezerra <roque.bezerra@ufsc.br>
2019-11-18 18:51:53 -0300

    Adiciona scheduler

--- src/main/java/br/com/roxs/domctba/crawler/DomCtbaCrawlerApplication.java
+++ src/main/java/br/com/roxs/domctba/crawler/DomCtbaCrawlerApplication.java
@@ -3,10 +3,12 @@ package br.com.roxs.domctba.crawler;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 import br.com.roxs.domctba.crawler.util.SSLRelax;
 
 @SpringBootApplication
+@EnableScheduling
 public class DomCtbaCrawlerApplication {
 
 	static {
